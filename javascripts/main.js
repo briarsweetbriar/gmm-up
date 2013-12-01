68732949941fac1a494976c11271d321a5290e41
timothythehuman <timothythehuman@gmail.com>
2013-12-01 00:51:47 -0600

    more minor stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -62,12 +62,12 @@ function initialize() {
     $.each(data, function(type, color){
       $("#map_interface_options").append("<li>\
         <span class='color-box' style='background: "+color+";'></span>\
-        <input type='checkbox' data-type='"+type+"' class='map-option'>"+type+"\
+        <input type='checkbox' data-typer='"+type+"' class='map-option'>"+type+"\
       </li>");
     });
 
     $( ".map-option" ).click(function() {
-      handleOptionToggle( $(this).data('type') )
+      handleOptionToggle( $(this).data('typer') )
     });
   });
 }
